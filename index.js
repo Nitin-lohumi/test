2f90647c955daf8c18cc7a8cc54d01cca6b83ab1
Nitin Lohumi <140873160+Nitin-lohumi@users.noreply.github.com>
2025-11-15 08:56:50 +0000

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Docker + CI/CD!");
+    res.send("hello this is docker ci/cd running port");
 });
 
 app.listen(3000, () => console.log("Server running on 3000"));
